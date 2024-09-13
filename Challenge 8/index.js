411800993be7d916264f7bfad459254fda47186c
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-13 23:38:32 +0500

    update

--- Challenge 8/index.js
+++ Challenge 8/index.js
@@ -1,10 +1,8 @@
 import { studentsArr } from './data.js'
 
 function calculateClassAverage(studentsArr) {
-  const cal = studentsArr.reduce((total,current)=>{
-    return total + current.grade
-  },0)
-  return cal
+  const cal = studentsArr.reduce((total,current)=> total + current.grade,0)
+  return cal/studentsArr.length
 }
 
 console.log(calculateClassAverage(studentsArr))

100534720f95e28ad8a238ac14629f684ee936de
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-13 21:40:01 +0500

    done

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const dreamHoliday = {
-  destination: '',
-  activity: '',
-  accommodation: '',
-  companion: ''
+  destination: 'Baku',
+  activity: 'Watch the F1 race and Quali',
+  accommodation: 'F1 Paddock',
+  companion: 'Josh revell'
 }
+const {destination,activity,accommodation,companion}=dreamHoliday
+document.body.innerText=`I am in ${destination}, for ${activity} staying in ${accommodation}, wiht ${companion}`
 
 /* 
 Challenge
